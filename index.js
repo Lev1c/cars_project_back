334c192664286362ea1cb671d6d00882a576103f
lev1c <dvauck2@gmail.com>
2025-07-04 15:41:25 +0400

    fix

--- index.js
+++ index.js
@@ -9,22 +9,28 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.post("/api", async (req, res) => {
-  const { a, u, p, srv } = req.body;
+  const { a, u, p, srv, sid, uid, aid, rid, from, to } = req.body;
 
   try {
+    const params = new URLSearchParams();
+    if (a) params.append("a", a);
+    if (u) params.append("u", u);
+    if (p) params.append("p", p);
+    if (srv) params.append("srv", srv);
+    if (sid) params.append("sid", sid);
+    if (uid) params.append("uid", uid);
+    if (aid) params.append("aid", aid);
+    if (rid) params.append("rid", rid);
+    if (from) params.append("from", from);
+    if (to) params.append("to", to);
+
     const proxyRes = await fetch("https://gps-it.ru/proxy.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: new URLSearchParams({
-        a,
-        u,
-        p,
-        srv,
-      }),
+      body: params,
     });
 
     const text = await proxyRes.text();
